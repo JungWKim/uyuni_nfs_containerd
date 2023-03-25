4104bf14b76541dc60ddbea29305a585ff0d08dd
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2023-03-25 21:37:15 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -137,6 +137,11 @@ sudo apt-get update \
 sudo mv ~/xiilab/config.toml /etc/containerd/
 sudo systemctl restart containerd
 
+# install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
 # install helmfile
 wget https://github.com/helmfile/helmfile/releases/download/v0.150.0/helmfile_0.150.0_linux_amd64.tar.gz
 tar -zxvf helmfile_0.150.0_linux_amd64.tar.gz
