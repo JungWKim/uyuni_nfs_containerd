f054e3e2ad40cd419eaa5aa4d58d0af7d05dd8f9
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2023-04-01 17:10:51 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -45,12 +45,12 @@ sudo update-initramfs -u
 sudo rmmod nouveau
 
 if [ ${LOCAL_FILE_COPY} == "yes" ] ; then
-	scp root@192.168.1.59:/root/files/NVIDIA-Linux-x86_64-515.57.run .
+	scp root@192.168.1.59:/root/files/NVIDIA-Linux-x86_64-525.89.02.run .
 else
-        wget https://kr.download.nvidia.com/XFree86/Linux-x86_64/515.57/NVIDIA-Linux-x86_64-515.57.run
+        wget https://kr.download.nvidia.com/XFree86/Linux-x86_64/525.89.02/NVIDIA-Linux-x86_64-525.89.02.run
 fi
 
-sudo sh ~/NVIDIA-Linux-x86_64-515.57.run
+sudo sh ~/NVIDIA-Linux-x86_64-525.89.02.run
 
 nvidia-smi
 nvidia-smi -L
