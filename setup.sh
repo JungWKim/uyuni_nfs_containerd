809f11b031e97a7d0ec7bc2b85f9f67e3ae49d38
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2023-04-04 00:37:24 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -163,8 +163,6 @@ rm LICENSE && rm README.md && rm helmfile_0.150.0_linux_amd64.tar.gz
 git clone -b develop https://github.com/xiilab/Uyuni_Deploy.git
 
 cd ~/Uyuni_Deploy
-sed -i "s/default.com/${IP}/g" environments/default/values.yaml
-sed -i "s/192.168.1.210/{}/g" environments/default/values.yaml
 sed -i "s/192.168.56.13/${NFS_IP}/g" environments/default/values.yaml
 sed -i "s:/kube_storage:${NFS_PATH}:g" environments/default/values.yaml
 sed -i "s/192.168.56.11/${IP}/g" environments/default/values.yaml
