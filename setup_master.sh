14434b10c0634b5f80529f13178b29cebbb0e1a2
JungWKim <lionmini@naver.com>
2023-04-05 19:37:48 +0900

    .

--- setup_master.sh
+++ setup_master.sh
@@ -90,25 +90,25 @@ sudo sysctl --system
 #----------------------------------------------------------------
 
 # install containerd
-#sudo apt-get update
+sudo apt-get update
 
-#sudo apt-get install \
-#    ca-certificates \
-#    curl \
-#    gnupg \
-#    lsb-release
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
-#sudo mkdir -p /etc/apt/keyrings
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-#echo \
-#  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-#sudo apt-get update \
-#    && sudo apt-get install -y containerd.io
+sudo apt-get update \
+    && sudo apt-get install -y containerd.io
 
-#sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo containerd config default | sudo tee /etc/containerd/config.toml
 
 # ssh configuration
 ssh-keygen -t rsa
