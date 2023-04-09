7a46804ea612a28dd84545e48c40d9591bf28f61
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2023-04-09 23:41:15 +0900

    Update setup_worker.sh

--- setup_worker.sh
+++ setup_worker.sh
@@ -29,3 +29,21 @@ net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
 sudo sysctl --system
+
+# enable kubectl & kubeadm auto-completion
+echo "source <(kubectl completion bash)" >> ${HOME}/.bashrc
+echo "source <(kubeadm completion bash)" >> ${HOME}/.bashrc
+
+echo "source <(kubectl completion bash)" | sudo tee -a /root/.bashrc
+echo "source <(kubeadm completion bash)" | sudo tee -a /root/.bashrc
+
+# install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# install helmfile
+wget https://github.com/helmfile/helmfile/releases/download/v0.150.0/helmfile_0.150.0_linux_amd64.tar.gz
+tar -zxvf helmfile_0.150.0_linux_amd64.tar.gz
+sudo mv helmfile /usr/bin/
+rm LICENSE && rm README.md && rm helmfile_0.150.0_linux_amd64.tar.gz
